ad394020db2c99401edc65a5d4818307a73f2a3f
tsetsen <tse9406@gmail.com>
2025-07-16 13:57:34 +0800

    update

--- be-food-delivery/controllers/types/express/global.d.ts
+++ be-food-delivery/controllers/types/express/global.d.ts
@@ -1,11 +1,18 @@
-import type { File } from "multer";
+import type { Request } from "express";
 
 declare global {
   namespace Express {
+    interface MulterFile {
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      size: number;
+      buffer: Buffer;
+    }
+
     interface Request {
-      file?: File;
+      file?: MulterFile;
     }
   }
 }
-
-export {};
